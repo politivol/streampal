e8c0e3eb16d1b429ba3b6d308f222121efe5eb09
Zach Dean <161634563+politivol@users.noreply.github.com>
2025-08-20 17:33:38 -0500

    Fix search results click handling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -55,8 +55,8 @@ export default function Search({ onSearch, onSelect }) {
       {results.length > 0 && (
         <ul className="search-results">
           {results.map((r) => (
-            <li key={r.id}>
-              <sl-button variant="neutral" type="button" onClick={() => handleSelect(r)}>
+            <li key={r.id} onClick={() => handleSelect(r)}>
+              <sl-button variant="neutral" type="button">
                 {r.title}
               </sl-button>
             </li>
